b9a87a34b0b4d788be7638866c2587c4b87599b6
nicolasport <nicolasport@users.noreply.github.com>
2021-09-06 00:12:12 -0300

    73. Abriendo buscador de ficheros

--- client/src/components/User/AvatarForm/AvatarForm.js
+++ client/src/components/User/AvatarForm/AvatarForm.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import './AvatarForm.scss'
 import { Button } from 'semantic-ui-react'
+import {useDropzone} from "react-dropzone"
 
 export default function AvatarForm(props) {
     const {setShowModal} = props
+
+    const onDrop = useCallback((acceptedFile) =>{
+        console.log(acceptedFile)
+    }, [])
+
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        multiple: false,
+        onDrop
+    })
     return (
         <div className="avatar-form">
-            <Button>Cargar una foto</Button>
+            <Button {...getRootProps()} >Cargar una foto</Button>
             <Button>Eliminar foto actual</Button>
             <Button onClick={() => setShowModal(false)}>Cancelar</Button>
+            <input {...getInputProps()} />
         </div>
     )
 }
